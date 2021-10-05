525c825b34408edab6a193cf1c1af89004c14cf7
rbogatyrev <74705623+rbogatyrev@users.noreply.github.com>
2021-10-05 10:36:32 +0400

    Added special symbol string generation method

--- src/test/java/com/wallarm/utils/RandomValuesGenerator.java
+++ src/test/java/com/wallarm/utils/RandomValuesGenerator.java
@@ -4,8 +4,10 @@ import com.wallarm.configuration.UserConfig;
 import org.aeonbits.owner.ConfigFactory;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.assertj.core.util.Strings;
 
 import java.util.GregorianCalendar;
+import java.util.Random;
 
 import static java.lang.String.format;
 
@@ -16,14 +18,28 @@ public class RandomValuesGenerator {
 
     public static String generateRandomString(int length) {
         return RandomStringUtils.randomAlphabetic(length).toLowerCase();
+
     }
 
     public static String generateRandomNumber(int length) {
         return RandomStringUtils.randomNumeric(length).toLowerCase();
     }
 
+    public static String generateSpecialCharactersString (int length){
+        Random random = new Random();
+        StringBuilder builder = new StringBuilder();
+        String specials = "~=+%^*/()[]{}/!@#$?|";
+        for (int i = 0; i < length; i++){
+            int number = random.nextInt(specials.length());
+            char ch = specials.charAt(number);
+            builder.append(ch);
+        }
+        return builder.toString();
+
+    }
+
     public static String generatePassword (){
-        String textPart = StringUtils.capitalize(generateRandomString(5) + RandomStringUtils.randomAscii(1));
+        String textPart = StringUtils.capitalize(generateRandomString(5) + generateSpecialCharactersString(1));
         String digitPart = generateRandomNumber(2);
         return textPart + digitPart;
     }
