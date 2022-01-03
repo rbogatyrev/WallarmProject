3bc4e0c86dc2c61447b8f7f0a58c9a45e05ed7da
rbogatyrev <74705623+rbogatyrev@users.noreply.github.com>
2022-01-03 21:39:05 +0400

    Fixed enum values

--- src/test/java/com/wallarm/enums/Infographics.java
+++ src/test/java/com/wallarm/enums/Infographics.java
@@ -2,6 +2,8 @@ package com.wallarm.enums;
 
 public enum Infographics implements IEnumValue{
     FINTECH_COMPANIES_INFOSECURITY("informacionnaya-bezopasnost-finteh-kompaniy"),
+    WAF_AND_API_PROTECTION("waf-and-api-protection-evaluation-checklist"),
+    WALLARM_AI_ENGINE("wallarm-ai-engine-how-it-works"),
     RUSSIAN_COMPANIES_CYBER_RISKS("kiber-riski-ugrozhayushchie-rossiyskim-kompaniyam") ;
 
     private String urlPart;
